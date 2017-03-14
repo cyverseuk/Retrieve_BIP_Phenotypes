447c0b38246f7b58175cb58474fcc65df06dd50d
teatree1212 <Annemarie.Eckes@earlham.ac.uk>
2017-03-14 11:36:31 +0000

    fix parameters in app .json and submission script.

--- agave/wrapper.sh
+++ agave/wrapper.sh
@@ -7,9 +7,13 @@ api_key="${param_2}"
 
 cp Phenotypes_Condor_submit.htc condorsubmit.htc
 #echo transfer_input_files = ${input_1} >> condorsubmit.htc
-echo arguments = ${param_1}  >> condorsubmit.htc
-echo arguments = ${param_2} >> condorsubmit.htc
-echo transfer_output_files = Seq_names.txt, Phenotypes_BIP_${param_1}.csv
+echo arguments = ${trial_name}  >> condorsubmit.htc
+echo arguments = ${api_key} >> condorsubmit.htc
+echo transfer_output_files = Seq_names.txt, Phenotypes_BIP_${trial_name}.csv
+echo should_transfer_files = YES
+echo when_to_transfer_output = ON_EXIT
+
+
 #H5COMMA=`echo ${h5} | sed -e 's/ /,/g'`
 #echo transfer_input_files = ${adapters},$H5COMMA >> sratools_submit.htc
 echo queue >> condorsubmit.htc
