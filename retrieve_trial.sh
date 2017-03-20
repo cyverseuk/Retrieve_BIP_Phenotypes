159a7606967ce4160d3f01f97914c68c899665a6
teatree1212 <Annemarie.Eckes@earlham.ac.uk>
2017-03-20 13:11:05 +0000

    use absolute path, might solve file not found issue

--- retrieve_trial.sh
+++ retrieve_trial.sh
@@ -6,7 +6,7 @@ api_key=$2
 echo "Retrieving Information for trial ${trial_name} from BIP"
 #make sure order of inputs is as in .ruby client script:
 
-ls
+
 
 echo "Start running ruby client"
 
@@ -16,7 +16,7 @@ ruby /usr/src/app/retrieve_phenotypes_and_ID.rb $trial_name $api_key
 echo "creating list of Sequence Identifiers (Seq_names.txt)"
 
 
-./ExtractSeqIds.sh ${trial_name}
+/usr/src/app/ExtractSeqIds.sh ${trial_name}
 
 
 echo "done"
