666c546a933569cd76417993d6ea30a9ec806a24
teatree1212 <Annemarie.Eckes@earlham.ac.uk>
2017-03-20 10:59:55 +0000

    add Sequence_IDs_log.txt ad outputfile from condor run

--- agave/wrapper.sh
+++ agave/wrapper.sh
@@ -8,7 +8,7 @@ api_key="${param_2}"
 cp Phenotypes_Condor_submit.htc condorsubmit.htc
 #echo transfer_input_files = ${input_1} >> condorsubmit.htc
 echo arguments = "${trial_name} ${api_key}"  >> condorsubmit.htc
-echo transfer_output_files = Seq_names.txt, Phenotypes_BIP_${trial_name}.csv
+echo transfer_output_files = Seq_names.txt,Phenotypes_BIP_${trial_name}.csv,Sequence_IDs_log.txt
 echo should_transfer_files = YES
 echo when_to_transfer_output = ON_EXIT
 
